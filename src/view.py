9a727f714cdee3e949ac893e04457b65d9ba2d19
Joseph Monighan <josephrodney@hotmail.com>
2026-01-11 13:51:52 -0500

    Add line break option to print line in view
    Change linebreak code for consistency

--- src/view.py
+++ src/view.py
@@ -15,7 +15,10 @@ class View:
         self.CONSOLE = Console()
         self.history = InMemoryHistory()
 
-    def print(self, message: str | list[str]) -> None:
+    def print(self, message: str | list[str], line_break: bool = False) -> None:
+        if line_break:
+            self.CONSOLE.print("\n")
+
         if type(message) is list:
             for i in message:
                 self.CONSOLE.print(i)
@@ -23,18 +26,14 @@ class View:
             self.CONSOLE.print(message)
 
     def print_system_message(self, message: str | list[str], line_break: bool = False):
-        def print_message(text: str | list[str]) -> None:
-            match line_break:
-                case True:
-                    self.CONSOLE.print(f"\n[bold cyan][*][/bold cyan] {text}")
-                case _:
-                    self.CONSOLE.print(f"[bold cyan][*][/bold cyan] {text}")
+        if line_break:
+            self.CONSOLE.print("\n")
 
         if type(message) is list:
             for m in message:
-                print_message(m)
+                self.CONSOLE.print(f"[bold cyan][*][/bold cyan] {m}")
         else:
-            print_message(message)
+            self.CONSOLE.print(f"[bold cyan][*][/bold cyan] {message}")
 
     def print_header_panel(self, model: str, search_model: str) -> None:
         self.CONSOLE.print(
